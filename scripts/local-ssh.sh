edd178331d574708acfa6db57c2239235634a091
Murod <parmonov98@yandex.ru>
2022-04-01 20:04:56 +0500

    local-ssh ssh config text added

--- scripts/local-ssh.sh
+++ scripts/local-ssh.sh
@@ -2,6 +2,15 @@
 EXPECTED_ARGS=2
 SERVER_USER=$1
 SERVER_IP=$2
-ssh-keygen -t rsa -b 4096 -f /home/$SERVER_USER/.ssh/$SERVER_USER -q -P ""
+ssh-keygen -t rsa -b 4096 -f /home/dev/.ssh/$SERVER_USER -q -P ""
 ssh $SERVER_USER@$SERVER_IP "mkdir -p /home/$SERVER_USER/.ssh/" 
-    && scp -pr /home/$SERVER_USER/.ssh/$SERVER_NAME.pub $SERVER_USER@$SERVER_IP:/home/$SERVER_USER/.ssh/authorized_keys
+    && scp -pr /home/dev/.ssh/$SERVER_NAME.pub $SERVER_USER@$SERVER_IP:/home/$SERVER_USER/.ssh/authorized_keys
+
+echo "
+Host $SERVER_USER
+Hostname $SERVER_IP
+User $SERVER_USER
+IdentityFile ~/.ssh/
+ServerAliveInterval 60
+ServerAliveCountMax 120" >> /home/dev/.ssh/config
+
--- scripts/server-ssh.sh
+++ scripts/server-ssh.sh
@@ -1,1 +1,6 @@
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+cd .ssh/
+chmod 600 authorized_keys
+sudo chattr +i authorized_keys
+cd ..
+chmod 700 .ssh
