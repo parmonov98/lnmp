ac601f507fef06190e9482381d06c616439cd120
Murod <parmonov98@yandex.ru>
2022-04-03 12:16:52 +0500

    create mysql db and user query

---
+++ scripts/mysql/user-database.sql
@@ -0,0 +1,6 @@
+create database db_name;
+grant all privileges on db_name.* to 'db_user'@'localhost' identified by 'password';
+flush privileges;
+
+
+create database toirjon; grant all privileges on toirjon.* to 'toirjon'@'localhost' identified by 'uJT`Cb?U#](ZtPdK_3Ws'; flush privileges;
